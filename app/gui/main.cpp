f4708e121dc838f4a6f069a014521763a7ff05d9
Llam <cutelaura@foxmail.com>
2026-08-09 13:31:21 +0900

    Guard GUI before Qt initialization

--- app/gui/main.cpp
+++ app/gui/main.cpp
@@ -42,11 +42,6 @@ class GuiInstanceGuard final {
 #endif
 
 int main(int argc, char* argv[]) {
-  QGuiApplication application(argc, argv);
-  QGuiApplication::setApplicationName(QStringLiteral("System Audio Route"));
-  QGuiApplication::setOrganizationName(QStringLiteral("System Audio Route"));
-  QQuickStyle::setStyle(QStringLiteral("Basic"));
-
 #ifdef Q_OS_WIN
   GuiInstanceGuard instance;
   if (!instance.valid()) {
@@ -56,6 +51,11 @@ int main(int argc, char* argv[]) {
     return 0;
   }
 #endif
+
+  QGuiApplication application(argc, argv);
+  QGuiApplication::setApplicationName(QStringLiteral("System Audio Route"));
+  QGuiApplication::setOrganizationName(QStringLiteral("System Audio Route"));
+  QQuickStyle::setStyle(QStringLiteral("Basic"));
 
   sar::gui::EngineController engine_controller;
   QQmlApplicationEngine engine;
